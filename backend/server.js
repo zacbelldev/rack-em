a9f882c5b83145d64ef2350d879f3bc587e5e5da
Zac Bell <zchrbell7@gmail.com>
2018-12-08 15:25:29 -0700

    mongo update

--- backend/server.js
+++ backend/server.js
@@ -11,30 +11,23 @@ const router = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/issues", { useNewUrlParser: true });
-
-
-
-
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://zacbelldev:PASSWORD@rackemup-l4tpf.gcp.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
- // perform actions on the collection object
-  client.close();
+mongoose.connect("mongodb+srv://rackemuser:PASSWORD@rackemup-l4tpf.gcp.mongodb.net/test?retryWrites=true", {
+  useNewUrlParser: true
 });
 
 
 
 
-
-
-
-
-
-
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://rackemuser:<PASSWORD>@rackemup-l4tpf.gcp.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("rackemup").collection("draw-pile");
+  // perform actions on the collection object
+  const testData = collection.body;
+  console.log(testData);
+  client.close();
+});
 
 
 
@@ -43,65 +36,69 @@ client.connect(err => {
 const connection = mongoose.connection;
 
 connection.once('open', () => {
-    console.log('MongoDB Database connection established succesfully');
+  console.log('MongoDB Database connection established succesfully');
 });
 
 router.route('/issues').get((req, res) => {
-    Issue.find((err, issues) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(issues);
-    });
+  Issue.find((err, issues) => {
+    if (err)
+      console.log(err);
+    else
+      res.json(issues);
+  });
 });
 
 router.route('/issues/:id').get((req, res) => {
-    Issue.findById(req.params.id, (err, issue) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(issue);
-    });
+  Issue.findById(req.params.id, (err, issue) => {
+    if (err)
+      console.log(err);
+    else
+      res.json(issue);
+  });
 });
 
 router.route('/issues/add').post((req, res) => {
-    let issue = new Issue(req.body);
-    issue.save()
-        .then(issue => {
-            res.status(200).json({'issue': 'Added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('Failed to create new record');
-        });
+  let issue = new Issue(req.body);
+  issue.save()
+    .then(issue => {
+      res.status(200).json({
+        'issue': 'Added successfully'
+      });
+    })
+    .catch(err => {
+      res.status(400).send('Failed to create new record');
+    });
 });
 
 router.route('/issues/update/:id').post((req, res) => {
-    Issue.findById(req.params.id, (err, issue) => {
-        if(!issue)
-            return next(new Error('Could not load document'));
-        else {
-            issue.title = req.body.title;
-            issue.responsible = req.body.responsible;
-            issue.description = req.body.description;
-            issue.severity = req.body.severity;
-            issue.status = req.body.status;
+  Issue.findById(req.params.id, (err, issue) => {
+    if (!issue)
+      return next(new Error('Could not load document'));
+    else {
+      issue.title = req.body.title;
+      issue.responsible = req.body.responsible;
+      issue.description = req.body.description;
+      issue.severity = req.body.severity;
+      issue.status = req.body.status;
 
-            issue.save().then(issue => {
-                res.json('Update done');
-            }).catch(err => {
-                res.status(400).send('Update failed');
-            });
-        }
-    });
+      issue.save().then(issue => {
+        res.json('Update done');
+      }).catch(err => {
+        res.status(400).send('Update failed');
+      });
+    }
+  });
 });
 
 router.route('/issues/delete/:id').get((req, res) => {
-    Issue.findByIdAndRemove({_id: req.params.id}, (err, issue) => {
-        if (err)
-            res.json(err);
-        else
-            res.json('Removed successfully');
-    });
+  Issue.findByIdAndRemove({
+    _id: req.params.id
+  }, (err, issue) => {
+    if (err)
+      res.json(err);
+    else
+      res.json('Removed successfully');
+  });
 });
 
 app.use('/', router);
